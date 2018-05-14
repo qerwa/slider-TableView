4802d11f5bf73dcc3aaa86712ed2f3ceadf1a11f
D7703_17 <d7703_17@d7703-17ui-imac.local>
2018-05-14 15:48:50 +0900

    add sliderTable

--- slider TableView/ViewController.swift
+++ slider TableView/ViewController.swift
@@ -8,16 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+      
+      
+      @IBOutlet weak var Mys: UISlider!
+      @IBOutlet weak var Myl: UILabel!
+      @IBOutlet weak var Myt: UITableView!
+      
       override func viewDidLoad() {
             super.viewDidLoad()
             // Do any additional setup after loading the view, typically from a nib.
+            Myt.delegate = self
+            Myt.dataSource = self
       }
 
-      override func didReceiveMemoryWarning() {
-            super.didReceiveMemoryWarning()
-            // Dispose of any resources that can be recreated.
+      @IBAction func slider(_ sender: Any) {
+            print("\(Mys.value) slider moved!")
+            Myl.text = String(Int(Mys.value))
+            Myt.reloadData()
+            }
+      
+      
+      func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return 100
+      }
+      
+      func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = Myt.dequeueReusableCell(withIdentifier: "re", for: indexPath)
+            cell.textLabel?.text = String(Int(Mys.value)+indexPath.row)
+            return cell
       }
 
 
